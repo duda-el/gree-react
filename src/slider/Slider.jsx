7e65b68cf32d57733f849e836cce078af5ba7757
Duda Elbakidze <dudaelbakidze94@gmail.com>
2024-06-17 01:57:55 +0400

    add aos

--- src/slider/Slider.jsx
+++ src/slider/Slider.jsx
@@ -1,96 +1,72 @@
-import React from 'react';
-// Import Swiper React components
+import React, { useEffect } from 'react'; // Import useEffect
 import { Swiper, SwiperSlide } from 'swiper/react';
 import useIsMobile from "../hooks/resize";
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
-import './Slider.css'
+import './Slider.css';
+import 'aos/dist/aos.css'; // Import AOS styles
+import AOS from 'aos'; // Import AOS library
 
-
-// import required modules
 import { Autoplay, Pagination } from 'swiper/modules';
 import conditioner1 from '../img/conditioner1.webp';
 import conditioner2 from '../img/conditioner2.webp';
 import conditioner3 from '../img/conditioner3.webp';
 
-// begin::import motion framer
-import { motion } from "framer-motion"
-
-
 export default function App() {
   const isMobile = useIsMobile();
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Customize the animation duration here
+      once: true, // Animation should happen only once
+    });
+
+    // Trigger AOS refresh after loading
+    setTimeout(() => {
+      AOS.refresh();
+    }, 100);
+  }, []);
+
   return (
-    <>
-      <motion.div initial={{ scale: 0.8 }} animate={{ y: 20, scale: 1 }} transition={{ type: "spring" }}>
-        <Swiper
-          spaceBetween={30}
-          centeredSlides={true}
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
-          }}
-          pagination={{
-            clickable: true,
-          }}
+    <div className="app-container">
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Autoplay, Pagination]}
+        className="mySwiper"
+        data-aos="zoom-in"
+      >
+        <SwiperSlide className='slide'>
+          {
+            isMobile
+              ? <img src={conditioner1} alt="conditioner" />
+              : <img src={conditioner1} alt="conditioner" />
+          }
+        </SwiperSlide>
 
-          modules={[Autoplay, Pagination]}
-          className="mySwiper"
-        >
+        <SwiperSlide className='slide'>
+          {
+            isMobile
+              ? <img src={conditioner2} alt="conditioner" />
+              : <img src={conditioner2} alt="conditioner" />
+          }
+        </SwiperSlide>
 
-          <SwiperSlide className='slide'>
-            {
-              isMobile
-              ?
-              (
-                <img src={conditioner1} alt="conditioner" />
-              )
-              :
-              (
-                <>
-                 <img src={conditioner1} alt="conditioner" />
-           
-                </>
-              )
-            }
-          </SwiperSlide>
-          
-          <SwiperSlide className='slide'>
-            {
-              isMobile
-              ?
-              (
-                <img src={conditioner2} alt="conditioner" />
-              )
-              :
-              (
-                <>
-                 <img src={conditioner2} alt="conditioner" />
-             
-                </>
-              )
-            }
-          </SwiperSlide>
-
-          <SwiperSlide className='slide'>
-            {
-              isMobile
-              ?
-              (
-                <img src={conditioner3} alt="conditioner" />
-              )
-              :
-              (
-                <>
-                 <img src={conditioner3} alt="conditioner" />
-                 
-                </>
-              )
-            }
-          </SwiperSlide>
-
-        </Swiper>
-      </motion.div>
-    </>
+        <SwiperSlide className='slide'>
+          {
+            isMobile
+              ? <img src={conditioner3} alt="conditioner" />
+              : <img src={conditioner3} alt="conditioner" />
+          }
+        </SwiperSlide>
+      </Swiper>
+    </div>
   );
 }
