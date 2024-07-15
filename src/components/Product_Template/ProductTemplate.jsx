1bf6fd4549e372724cb05aaa3831d6ed9c0de81f
Duda Elbakidze <dudaelbakidze94@gmail.com>
2024-07-15 12:54:02 +0400

    added ProductTemplate

---
+++ src/components/Product_Template/ProductTemplate.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer";
+import "ProductTemplate.css"
+
+function ProductTemplate() {
+  return (
+    <>
+    <Header/>
+    <Footer/>
+    </>
+  );
+}
+
+export default ProductTemplate;
